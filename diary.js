6d422a7db4df5735d7c5de58e49c4ca5d2ae91c6
Rosa Kang <therosessom@gmail.com>
2021-05-09 10:40:59 -0700

    first commit

---
+++ diary.js
@@ -0,0 +1,30 @@
+//1. Filter: Filter through the list of cities and return the cities with a length greater than 6
+let cities =['Toronto', 'Vancouver', 'Nyc', 'Dubai', 'LA', 'Austin'];
+let result = cities.filter(city=> city.length > 6);
+// let result = cities.filter((city) => {city.length > 6}); --> wrong because of {}
+console.log(result);
+
+//2. MAP : Map through the famousPeople array and only return the first and last names
+const famousPeople = [
+    {first: 'Oprah', last: 'Winfrey', city:'Chicago'},
+    {first:'Barak', last:'Obama', city:'Washington'},
+    {first:'Ariana', last:'Grande', city:'L.A'}
+];
+
+
+ //3. REDUCE: sum up all numbers in the array
+ let array = [10,12,23,34];
+ 
+//4. Foreach: Add on text 'Day of the week' to all of the days in the arr
+let arr = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+
+
+
+//자료구조와 알고리즘 (검색, 삽입, 정렬, 삭제)
+//Array is a row of data which has an index.
+ 'user strict';
+ //1. Array Declaration 방법
+const arr1 = new Array();
+const arr2 = [1,2];
+
+
---
+++ es6_array.js
@@ -0,0 +1,51 @@
+//1. Filter  : filter the city with a length greater than 6
+// * find method returns the first item that fulfill the condition whereas, filter will return every items matches requirement
+
+// let cities = ['Toronto', 'Vancouver', 'Nyc', 'Dubai', 'LA', 'Austin']
+// let lengthy = cities.filter((city)=> city.length > 6);
+//  console.log(lengthy);
+
+ //2. MAP : Map through the famous people and return the first and last names.
+
+//  const famousPeople = [
+//      {
+//          first: "Oprah", last:"Winfrey" , city: "Chicago"
+//      }, 
+//      {
+//          first: "Barak", last:"Obama" , city: "Washington"
+//      } ,
+//      {
+//          first: "Ariana", last:"Grande" , city: "LA"
+//      }
+//  ]
+
+// const fullName = famousPeople.map(people => `${people.first} ${people.last}`);
+// console.log(fullName);
+
+// 3. REDUCE : Sum up all numbers in the array
+
+// let arr2 = [10,12,23,34];
+// let result = arr2.reduce((prev, curr) => 
+//     prev + curr
+// , 0);
+// console.log(result);
+
+//4. For each : Add on text 'Day of the week' all of the days in the arr
+
+// let arr3 = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+// let weekDay = arr3.map((day) => `${day} of the week`);
+// let weekDays = arr3.forEach((day) => {
+//     console.log(`${day} of the Week`);
+// })
+// console.log(weekDay);
+// console.log(arr3);
+
+//5. Ascending order (Sort ();)
+let Stu = [8,5,6,9,3,8,2,4,6,10,8,5,6,1,7,10,5,3,7,6]
+// let newStu = Stu.reduce((prev, curr)=> prev+curr, 0);
+// let average = newStu/Stu.length;
+// console.log(average); 
+
+let ascending = Stu.sort((a, b) => a - b);
+console.log(ascending);
+console.log(Math.max(...ascending));
